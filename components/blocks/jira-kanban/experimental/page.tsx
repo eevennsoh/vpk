e01a44a010b5427d30decc48f1723dca71104e9c
Ee Venn Soh <esoh@atlassian.com>
2026-09-15 05:37:16 +1000

    Keep session column geometry stable

--- components/blocks/jira-kanban/experimental/page.tsx
+++ components/blocks/jira-kanban/experimental/page.tsx
@@ -991,7 +991,7 @@ function ExperimentalJiraKanbanPageContent({
 										: undefined,
 									sessionDrag: boardSessionDrag.untrackedBinding,
 								}}
-								className={isListContent ? "pb-4 md:pb-5" : "pb-3"}
+								className="pb-3"
 								columnFrame={columnChromeStyles.headerFrame}
 								paddingTop={withKanbanDropContentGutter(0, columnChromeStyles).paddingTop}
 								sessionFlyoutsSuspended={boardSessionDrag.transaction !== null}
